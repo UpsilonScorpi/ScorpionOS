8e67fff0697791d522326c1ff48b017ced7fe922
Upsilon Scorpi <upsilonscorpi4677@gmail.com>
2026-02-13 10:41:01 +0100

    try for batch multiple targets

---
+++ ScorpionOS/functions/hacking-batch-multiple.js
@@ -0,0 +1,156 @@
+/**
+ * Hack manager
+ * @param {NS} ns
+ */
+import { scanAll, gainAccess, checkBackdoors } from "../tools/utils.js";
+
+export async function main(ns) {
+  ns.disableLog("ALL");
+  let targets = [];
+
+  const cooldown = 200;
+  const sleepTime = 500;
+  const hackAmount = 0.9;
+
+  while (true) {
+    manageServer(ns);
+    const servers = scanAll(ns);
+    gainAccess(ns, servers);
+    const list = checkBackdoors(ns, servers);
+    for (const server of list) {
+      ns.toast("🔑 Backdoors possibles :" + server, "warning", 5000);
+    }
+    const targetsNew = bestTarget(ns, servers);
+    if (!sameTargets(targetsNew,targets)) {
+      targets = targetsNew;
+      for (const p of ns.ps()) if (p.filename === "ScorpionOS/workers/batch.js") ns.kill(p.pid);
+      for (const target of targets) ns.exec("ScorpionOS/workers/batch.js","home",1,target[0],cooldown,sleepTime,hackAmount);
+    }
+    await ns.sleep(5000);
+  }
+}
+
+/**
+ * Get best target
+ */
+function bestTarget(ns, servers) {
+  let targets = [];
+  const pserv = ns.getPurchasedServers();
+  const playerHackLevel = ns.getHackingLevel();
+  const hackMult = ns.getHackingMultipliers();
+  const player = ns.getPlayer();
+  for (const s of servers) {
+    if (s === "home") continue;
+    if (pserv.includes(s)) continue;
+    if (!ns.hasRootAccess(s)) continue;
+
+    const reqHackLevel = ns.getServerRequiredHackingLevel(s);
+    if (reqHackLevel > playerHackLevel) continue;
+
+    const maxMoney = ns.getServerMaxMoney(s);
+    if (maxMoney <= 0) continue;
+
+    const score = getScore(ns, s, {
+      maxMoney,
+      playerHackLevel,
+      hackMult,
+      reqHackLevel,
+      player
+    });
+
+    targets.push([s,score]);
+  }
+  targets.sort((a, b) => b[1] - a[1]);
+  return targets.slice(0,25);
+}
+
+/**
+ * Calculate the score of a target
+ */
+function getScore(ns, s, data) {
+  const { maxMoney, playerHackLevel, hackMult, reqHackLevel, player } = data;
+
+  const hasFormulas = ns.fileExists("Formulas.exe", "home");
+  if (!hasFormulas) {
+    const weakenTime = ns.getWeakenTime(s);
+
+    const minSec = ns.getServerMinSecurityLevel(s);
+
+    const hackChance = Math.min(1, Math.max(0, (playerHackLevel / (playerHackLevel + reqHackLevel * minSec)) * hackMult.chance));
+    const hackPercent = Math.min(1, Math.max(0, (0.002 * (playerHackLevel / reqHackLevel)) / minSec));
+
+    if (hackChance <= 0 || hackPercent <= 0) return 0;
+
+    const score = (maxMoney * hackPercent * hackMult.money * hackChance) / weakenTime;
+
+    return score;
+  }
+  else {
+    const sim = ns.getServer(s);
+    sim.hackDifficulty = sim.minDifficulty;
+    sim.moneyAvailable = sim.moneyMax;
+    const hackChance = ns.formulas.hacking.hackChance(sim, player);
+    const hackPercent = ns.formulas.hacking.hackPercent(sim, player);
+    const weakenTime = ns.formulas.hacking.weakenTime(sim, player);
+
+    if (hackChance <= 0 || hackPercent <= 0) return 0;
+
+    const score = (maxMoney * hackPercent * hackChance) / weakenTime;
+
+    return score;
+  }
+}
+
+/**
+ * Buy / Upgrade private server
+ */
+function manageServer(ns) {
+  const pservs = ns.getPurchasedServers();
+  const maxServers = ns.getPurchasedServerLimit();
+  const maxRam = ns.getPurchasedServerMaxRam();
+  const playerMoney = ns.getServerMoneyAvailable("home");
+  const prefix = "pserv-";
+
+  let ram = 1;
+  while (ram * 2 <= maxRam && ns.getPurchasedServerCost(ram * 2) < playerMoney) {
+    ram *= 2;
+  }
+
+  if (ram === 1) return;
+
+  let name = "";
+
+  if (pservs.length < maxServers) {
+    name = prefix + pservs.length;
+    ns.purchaseServer(name, ram);
+    return;
+  }
+
+  let weakest = null;
+  let weakestRam = 0;
+  for (const s of pservs) {
+    if (ns.getServerMaxRam(s) < weakestRam && ns.ps(s).length === 0) {
+      weakest = s;
+      weakestRam = ns.getServerMaxRam(s);
+    }
+  }
+
+  if (weakestRam < ram && weakest !== null) {
+    ns.killall(weakest);
+    if (ns.deleteServer(weakest)) {
+      name = weakest;
+      ns.purchaseServer(weakest, ram);
+    }
+  }
+}
+
+/**
+ * Check if same targets
+ */
+function sameTargets(a, b) {
+    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i][0] !== b[i][0]) return false;
+    }
+    return true;
+}
---
+++ ScorpionOS/workers/batch.js
@@ -0,0 +1,166 @@
+/**
+ * Hack manager
+ * @param {NS} ns
+ */
+
+import { scanAll } from "../tools/utils";
+
+export async function main(ns) {
+  ns.disableLog("ALL");
+  const target = ns.args[0];
+  const cooldown = ns.args[1];
+  const sleepTime = ns.args[2];
+  const hackAmount = ns.args[3];
+
+  const weakenScript = "ScorpionOS/workers/weaken.js";
+  const growScript = "ScorpionOS/workers/grow.js";
+  const hackScript = "ScorpionOS/workers/hack.js";
+
+  const minSec = ns.getServerMinSecurityLevel(target);
+  const maxMoney = ns.getServerMaxMoney(target);
+
+  while (ns.getServerMoneyAvailable(target) < maxMoney * 0.99 || ns.getServerSecurityLevel(target) > minSec + 0.1) {
+    await prep(ns, {
+      target: target,
+      minSec,
+      maxMoney,
+      cooldown
+    });
+  }
+
+  const hackPercent = ns.hackAnalyze(target);
+  const hackThread = Math.ceil(hackAmount / hackPercent);
+
+  const secHack = ns.hackAnalyzeSecurity(hackThread);
+  const weakenHackThread = Math.ceil(secHack / ns.weakenAnalyze(1) * 1.1);
+
+  const growRatio = (1 + (hackAmount / (1 - hackAmount))) * 1.1;
+  const growThread = Math.ceil(ns.growthAnalyze(target, growRatio) * 1.1);
+
+  const secGrow = ns.growthAnalyzeSecurity(growThread);
+  const weakenGrowThread = Math.ceil(secGrow / ns.weakenAnalyze(1) * 1.1);
+
+  const tH = ns.getHackTime(target);
+  const tG = ns.getGrowTime(target);
+  const tW = ns.getWeakenTime(target);
+
+  const hackWait = tW - cooldown - tH;
+  const weakenHackWait = 0;
+  const growWait = tW - tG + cooldown;
+  const weakenGrowWait = 2 * cooldown;
+
+  while (true) {
+    deployWorkers(ns, {
+      target,
+      script: hackScript,
+      thread: hackThread,
+      delay: hackWait
+    });
+    deployWorkers(ns, {
+      target,
+      script: weakenScript,
+      thread: weakenHackThread,
+      delay: weakenHackWait
+    });
+    deployWorkers(ns, {
+      target,
+      script: growScript,
+      thread: growThread,
+      delay: growWait
+    });
+    deployWorkers(ns, {
+      target,
+      script: weakenScript,
+      thread: weakenGrowThread,
+      delay: weakenGrowWait
+    });
+    await ns.sleep(sleepTime);
+  }
+}
+
+/**
+ * Get all workers
+ */
+function listWorker(ns, servers) {
+  let workerList = [];
+  for (const s of servers) {
+    if (s === "home") continue;
+    if (!ns.hasRootAccess(s)) continue;
+
+    const ram = ns.getServerMaxRam(s);
+
+    if (ram < 2) continue;
+
+    workerList.push([s, ram]);
+  }
+  workerList.sort((a, b) => b[1] - a[1])
+  return workerList;
+}
+
+/**
+ * Function to prepare targeted server
+ */
+async function prep(ns, data) {
+  const { target, minSec, maxMoney, cooldown } = data;
+
+  const currentSec = ns.getServerSecurityLevel(target);
+  const secToRemove = currentSec - minSec;
+  const weakenThread1 = Math.ceil(secToRemove / ns.weakenAnalyze(1) * 1.1);
+
+  const currentMoney = ns.getServerMoneyAvailable(target);
+  const growRatio = maxMoney / Math.max(currentMoney, 1);
+  const growThread = Math.ceil(ns.growthAnalyze(target, growRatio) * 1.1);
+
+  const secGrow = ns.growthAnalyzeSecurity(growThread);
+  const weakenThread2 = Math.ceil(secGrow / ns.weakenAnalyze(1) * 1.1);
+
+  const tW = ns.getWeakenTime(target);
+  const tG = ns.getGrowTime(target);
+
+  const launchW1 = 0;
+  const launchG = tW - tG + cooldown;
+  const launchW2 = 2 * cooldown;
+
+  const weakenScript = "ScorpionOS/workers/weaken.js";
+  const growScript = "ScorpionOS/workers/grow.js";
+
+  if (weakenThread1 > 0) deployWorkers(ns, {
+    target,
+    script: weakenScript,
+    thread: weakenThread1,
+    delay: launchW1
+  });
+  if (growThread > 0) deployWorkers(ns, {
+    target,
+    script: growScript,
+    thread: growThread,
+    delay: launchG
+  });
+  if (weakenThread2 > 0) deployWorkers(ns, {
+    target,
+    script: weakenScript,
+    thread: weakenThread2,
+    delay: launchW2
+  });
+
+  await ns.sleep(tW + 2.5 * cooldown);
+}
+
+/**
+ * Function to deploy worker
+ */
+function deployWorkers(ns, data) {
+  const { target, script, thread, delay } = data;
+  const workers = listWorker(ns, scanAll(ns));
+  let remaining = thread;
+
+  for (const [server, ram] of workers) {
+    const free = Math.floor((ram - ns.getServerUsedRam(server)) / ns.getScriptRam(script));
+    if (free <= 0) continue;
+    const use = Math.min(free, remaining);
+    ns.scp(script, server);
+    ns.exec(script, server, use, target, delay);
+    remaining -= use;
+    if (remaining <= 0) break;
+  }
+}
